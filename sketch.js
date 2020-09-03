42474a333dfee6d29f00476e4f1a37cd7320738a
aaryan22332 <67644746+aaryan22332@users.noreply.github.com>
2020-09-03 15:58:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball1;
+var database,position
+
+
+function setup(){
+    createCanvas(500,500);
+   database=firebase.database();
+    ball1 = createSprite(250,250,10,10);
+    ball1.shapeColor = "red";
+    var ff = database.ref('ball/position')
+    ff.on("value",readposition,showerror)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+   database.ref('ball/position').set({
+       x:position.x+x,
+       y:position.y+y,
+   })
+}
+function readposition(data){
+position=data.val()
+ball1.x=position.x
+ball1.y=position.y;
+
+
+}
+function showerror(){
+    console.log("error")
